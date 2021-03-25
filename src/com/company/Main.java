ef0be51ba3e07b9196e14c34122edb70b26c840f
Wendell Jed Duaso <rawjwj020314@gmail.com>
2021-03-25 21:24:24 +0800

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    byte b = 127;
+    short s = 32767;
+    int i = 122;
+    long largeDecimalNumbers = -12;
+    float numberWithDecimalPlaces = 8.3f ;
+    double verylargeNumberWithDecimalPlaces = 12312312.1232;
+    char singleCharacter = '\u0041';
+    boolean isTrueorFalse = true;
+
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(i);
+    System.out.println(largeDecimalNumbers);
+    System.out.println(numberWithDecimalPlaces);
+    System.out.println(verylargeNumberWithDecimalPlaces);
+    System.out.println(singleCharacter);
+    System.out.println(isTrueorFalse);
+    }
+}
