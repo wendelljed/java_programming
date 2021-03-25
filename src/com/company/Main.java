305dbd25dd2f30a1092f6196789ad6ff9fa63895
Wendell Jed Duaso <rawjwj020314@gmail.com>
2021-03-25 21:39:27 +0800

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,5 +20,38 @@ public class Main {
     System.out.println(verylargeNumberWithDecimalPlaces);
     System.out.println(singleCharacter);
     System.out.println(isTrueorFalse);
+
+    //switch
+        int day = 7;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+        //for loops
+        String[] cars = {"Suzuki", "Toyota", "Mazda", "Nissan"};
+        for (String w : cars) {
+            System.out.println(w);
+        }
+        //while
+        //do..while
     }
 }
