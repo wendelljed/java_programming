85342e2bac3a06ce42f36ebaf4b3a480a13d6735
Wendell Jed Duaso <rawjwj020314@gmail.com>
2021-03-30 09:32:27 +0800

    7th commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -154,29 +154,14 @@ public class Main {
     //Switch
         int adlaw = 5;
 
-        switch (adlaw){
-
-        case 1:
-            System.out.println("Monday");
-            break;
-        case 2:
-            System.out.println("Tuesday");
-            break;
-        case 3:
-            System.out.println("Wednesday");
-            break;
-        case 4:
-            System.out.println("Thursday");
-            break;
-        case 5:
-            System.out.println("Friday");
-            break;
-        case 6:
-            System.out.println("Saturday");
-            break;
-        case 7:
-            System.out.println("Sunday");
-            break;
+        switch (adlaw) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
         }
     // While Loop
 
